3f95ebc706c6178b1589b4a04618811d1d06abc3
BermudoMan <ivan.kurochkin.95@bk.ru>
2023-12-17 19:19:35 +0300

    f version

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 URL = 'https://www.avito.ru/ivanovo/kvartiry/prodam-ASgBAgICAUSSA8YQ?s=104'
-TOKEN = '6825062405:AAHivnXgYeADZGOwcMpyzGQvfoBiVX5fU0Q'
+TOKEN = ''
 DRIVER_PATH = 'Z:\\webdrivers\\chromedriver-win64\\chromedriver.exe'
 BROWSER = 'Z:\\webdrivers\\chrome-win64'
 
 # db data
 database = 'HSearcher(bot)'
 user = 'postgres'
-password = '12345'
+password = ''
 host = ''
 port = '5432'
 
